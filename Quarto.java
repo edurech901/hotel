6da1a2fa4ec1c79181a3ec0b38ce8d6af5489d10
edurech901 <eduardo.rech901@gmail.com>
2024-10-21 20:25:47 -0300

    quarto 1

---
+++ Quarto.java
@@ -0,0 +1,54 @@
+public class Quarto {
+  int num_quarto;
+  int tipo_quarto;
+  double preco;
+  boolean disponibilidade;
+
+    public Quarto(int num_quarto, int tipo_quarto) {
+      this.num_quarto = num_quarto;
+      this.tipo_quarto = tipo_quarto;
+      if(tipo_quarto == 1) {
+        this.preco = 200;
+      }
+      else if(tipo_quarto == 2) {
+        this.preco = 350;
+      }
+      else if(tipo_quarto == 3) {
+        this.preco = 500;
+      }
+      this.disponibilidade = false; //false = livre
+    }
+  
+  public int getNum_quarto() {
+    return num_quarto;
+  }
+
+  public void setNum_quarto(int num_quarto) {
+    this.num_quarto = num_quarto;
+  }
+
+  public int getTipo_quarto() {
+    return tipo_quarto;
+  }
+
+  public void setTipo_quarto(int tipo_quarto) {
+    this.tipo_quarto = tipo_quarto;
+  }
+
+  public boolean isDisponibilidade() {
+    return disponibilidade;
+  }
+
+  public void setDisponibilidade(boolean disponibilidade) {
+    this.disponibilidade = disponibilidade;
+  }
+
+  public double getPreco() {
+    return preco;
+  }
+
+  public void setPreco(double preco) {
+    this.preco = preco;
+  }
+
+}
