bb3faf868debc2549374a02f4275d158c62f60e9
edurech901 <eduardo.rech901@gmail.com>
2024-10-22 20:21:27 -0300

    Reservas 2

--- Hotel.java
+++ Hotel.java
@@ -1,7 +1,7 @@
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 import java.util.Scanner;
-import java.util.Date;
 
 public class Hotel {
 
@@ -24,9 +24,11 @@ public class Hotel {
     return false;
   }
 
+  @SuppressWarnings("deprecation")
   public static void main(String[] args) {
 
     List<Quarto> quartos = quartos();
+    List<Reserva> reservas = new ArrayList<>();
 
     while (true) {
       Scanner scan = new Scanner(System.in);
@@ -63,6 +65,45 @@ public class Hotel {
           for(Quarto quarto: quartos){
             quarto.printar_quarto();
           }
+        }
+        case 3 -> {
+          String nome_hospede;
+          int dia, mes, ano;
+          Date checkin, checkout;
+          List<Quarto> quartos_reservados = new ArrayList<>();
+          System.out.println("Digite o nome da reserva: ");
+          nome_hospede = scan.next();
+          System.out.println("Digite o dia do check-in:");
+          dia = scan.nextInt();
+          System.out.println("Digite o mes do check-in:");
+          mes = scan.nextInt();
+          System.out.println("Digite o ano do check-in:");
+          ano = scan.nextInt();
+          checkin = new Date(ano, mes, dia);
+          System.out.println("Digite o nome da reserva?");
+          nome_hospede = scan.next();
+          System.out.println("Digite o dia do check-out:");
+          dia = scan.nextInt();
+          System.out.println("Digite o mes do check-out:");
+          mes = scan.nextInt();
+          System.out.println("Digite o ano do check-out:");
+          ano = scan.nextInt();
+          checkout = new Date(ano, mes, dia);
+          while(true){
+            int num_quarto;
+            System.out.println("Digite o numero do quarto:");
+            System.out.println("Digite '0' para sair:");
+            num_quarto = scan.nextInt();
+            if(num_quarto == 0) break;
+            else {
+              for(Quarto a : quartos){
+                if (a.getNum_quarto() == num_quarto){
+                  quartos_reservados.add(a);
+                }
+              }
+            }
+          }
+          reservas.add(new Reserva(nome_hospede, checkin, checkout, quartos_reservados));
         }
       }
     }
