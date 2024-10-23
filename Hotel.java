11490b56294e0425b4dbeeeb636ead4df168c4ed
edurech901 <eduardo.rech901@gmail.com>
2024-10-22 21:01:35 -0300

    Reservas 3

--- Hotel.java
+++ Hotel.java
@@ -17,9 +17,10 @@ public class Hotel {
     return quartos;
   }
 
-  public static boolean encontrar_num_quarto(List<Quarto> quartos, int num_quarto){
-    for(Quarto a : quartos){
-      if(num_quarto == a.getNum_quarto()) return true;
+  public static boolean encontrar_num_quarto(List<Quarto> quartos, int num_quarto) {
+    for (Quarto a : quartos) {
+      if (num_quarto == a.getNum_quarto())
+        return true;
     }
     return false;
   }
@@ -29,13 +30,16 @@ public class Hotel {
 
     List<Quarto> quartos = quartos();
     List<Reserva> reservas = new ArrayList<>();
+    reservas.add(new Reserva("Eduardo", new Date(2024, 10, 10), new Date(2024, 10, 26), quartos));
 
     while (true) {
       Scanner scan = new Scanner(System.in);
       int op;
       System.out.println("\n-------- MENU ---------");
       System.out.println("1  -- Cadastrar quarto");
-      System.out.println("2  -- lista de quartos");
+      System.out.println("2  -- Lista de quartos");
+      System.out.println("3  -- Fazer reserva");
+      System.out.println("4  -- Lista de reservas");
       System.out.println("0  -- Sair\n");
       System.out.println("Escolha a opção:");
       op = scan.nextInt();
@@ -54,7 +58,7 @@ public class Hotel {
           }
           System.out.println("Digite o codigo do tipo do quarto:");
           tipo_quarto = scan.nextInt();
-          if (tipo_quarto<1 || tipo_quarto > 3) {
+          if (tipo_quarto < 1 || tipo_quarto > 3) {
             System.out.println("Código de tipo de quarto invalido");
             break;
           }
@@ -62,7 +66,7 @@ public class Hotel {
           System.err.println("Quarto cadastrado com sucesso");
         }
         case 2 -> {
-          for(Quarto quarto: quartos){
+          for (Quarto quarto : quartos) {
             quarto.printar_quarto();
           }
         }
@@ -73,37 +77,44 @@ public class Hotel {
           List<Quarto> quartos_reservados = new ArrayList<>();
           System.out.println("Digite o nome da reserva: ");
           nome_hospede = scan.next();
-          System.out.println("Digite o dia do check-in:");
+          System.out.println("Digite o dia do check-in: ");
           dia = scan.nextInt();
-          System.out.println("Digite o mes do check-in:");
+          System.out.println("Digite o mes do check-in: ");
           mes = scan.nextInt();
-          System.out.println("Digite o ano do check-in:");
+          System.out.println("Digite o ano do check-in: ");
           ano = scan.nextInt();
           checkin = new Date(ano, mes, dia);
-          System.out.println("Digite o nome da reserva?");
-          nome_hospede = scan.next();
-          System.out.println("Digite o dia do check-out:");
+          System.out.println("Digite o dia do check-out: ");
           dia = scan.nextInt();
-          System.out.println("Digite o mes do check-out:");
+          System.out.println("Digite o mes do check-out: ");
           mes = scan.nextInt();
-          System.out.println("Digite o ano do check-out:");
+          System.out.println("Digite o ano do check-out: ");
           ano = scan.nextInt();
           checkout = new Date(ano, mes, dia);
-          while(true){
+          while (true) {
             int num_quarto;
             System.out.println("Digite o numero do quarto:");
             System.out.println("Digite '0' para sair:");
             num_quarto = scan.nextInt();
-            if(num_quarto == 0) break;
+            if (num_quarto == 0 && !quartos_reservados.isEmpty())
+              break;
             else {
-              for(Quarto a : quartos){
-                if (a.getNum_quarto() == num_quarto){
-                  quartos_reservados.add(a);
+              System.out.println("Ainda não foram adicionados quartos a reserva");
+              if (encontrar_num_quarto(quartos, num_quarto)) {
+                for (Quarto a : quartos) {
+                  if (a.getNum_quarto() == num_quarto) {
+                    System.out.println("Quarto " + num_quarto + " reservado");
+                    quartos_reservados.add(a);
+                  }
                 }
               }
             }
           }
           reservas.add(new Reserva(nome_hospede, checkin, checkout, quartos_reservados));
+        }
+        case 4 -> {
+          for (Reserva a : reservas)
+            a.printar_reserva();
         }
       }
     }
--- Reserva.java
+++ Reserva.java
@@ -6,11 +6,61 @@ public class Reserva {
   Date checkin, checkout;
   List<Quarto> quartos;
 
-  Reserva(String nome_hospede, Date checkin, Date checkout, List<Quarto> quartos){
+  Reserva(String nome_hospede, Date checkin, Date checkout, List<Quarto> quartos) {
     this.nome_hospede = nome_hospede;
     this.checkin = checkin;
     this.checkout = checkout;
     this.quartos = quartos;
   }
 
+  public String getNome_hospede() {
+    return nome_hospede;
+  }
+
+  public void setNome_hospede(String nome_hospede) {
+    this.nome_hospede = nome_hospede;
+  }
+
+  public Date getCheckout() {
+    return checkout;
+  }
+
+  public void setCheckout(Date checkout) {
+    this.checkout = checkout;
+  }
+
+  public Date getCheckin() {
+    return checkin;
+  }
+
+  public void setCheckin(Date checkin) {
+    this.checkin = checkin;
+  }
+
+  public List<Quarto> getQuartos() {
+    return quartos;
+  }
+
+  public void setQuartos(List<Quarto> quartos) {
+    this.quartos = quartos;
+  }
+
+  @SuppressWarnings("deprecation")
+  public void printar_reserva() {
+    System.out.println("\nNome do hospede: " + nome_hospede);
+    System.out.println("Periodo: ");
+    System.out.println(checkin.getDate() + "/" + checkin.getMonth() + "/" + checkin.getYear() + " - "
+        + checkout.getDate() + "/" + checkout.getMonth() + "/" + checkout.getYear());
+    System.out.println("Quartos reservados:");
+    for (Quarto a : this.quartos) {
+      System.out.println("  Numero do quarto: " + a.getNum_quarto());
+      System.out.println("  Tipo do quarto: ");
+      switch (a.getTipo_quarto()) {
+        case 1 -> System.out.println("  Solteiro - R$200,00");
+        case 2 -> System.out.println("  Casal - R$350,00");
+        case 3 -> System.out.println("  Casal + Solteiro - R$500,00");
+      }
+    System.out.println("-----------------------");
+    }
+  }
 }
